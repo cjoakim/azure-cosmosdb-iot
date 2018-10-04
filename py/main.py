f8dafcff16744f9ff8a1ad5c39118758cd186eae
Chris Joakim <christopher.joakim@gmail.com>
2018-10-04 11:48:21 -0400

    python sending date in EH msg

--- py/main.py
+++ py/main.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+import json
 import datetime
 import time
 import os
@@ -23,22 +23,36 @@ print('address:   {}'.format(address))
 print('user:      {}'.format(user))
 print('key:       {}'.format(key))
 
-try:
-    client = EventHubClient(address, debug=False, username=user, password=key)
-    sender = client.add_sender(partition="0")
-    client.run()
-    try:
-        start_time = time.time()
-        for i in range(10):
-            print("Sending message: {}".format(i))
-            sender.send(EventData(str(i)))
-    except:
-        raise
-    finally:
-        end_time = time.time()
-        client.stop()
-        run_time = end_time - start_time
-        print.info("Runtime: {} seconds".format(run_time))
+def read_json(infile):
+    with open(infile, 'rt') as f:
+        return json.loads(f.read())
 
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    count = int(sys.argv[1])
+
+    try:
+        cities = read_json('data/city_list.json')
+        print('cities: {}'.format(len(cities)))
+
+        client = EventHubClient(address, debug=False, username=user, password=key)
+        sender = client.add_sender(partition="0")
+        client.run()
+        try:
+            start_time = time.time()
+            for i in range(count):
+                msg = cities[i]
+                msg['pk'] = msg['name']
+                msg['time'] = str(datetime.datetime.today())
+                print("Sending message: {}".format(msg))
+                sender.send(EventData(str(msg)))
+                time.sleep(1)
+        except:
+            raise
+        finally:
+            end_time = time.time()
+            client.stop()
+            run_time = end_time - start_time
+            print("Runtime: {} seconds".format(run_time))
+
+    except KeyboardInterrupt:
+        pass
