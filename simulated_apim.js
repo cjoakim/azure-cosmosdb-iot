85260a1c84e433d1ea2a2e3bd8fbb37e94f3b539
Chris Joakim <christopher.joakim@gmail.com>
2018-10-04 10:24:33 -0400

    similated_apim.js sending messages to evthub

--- simulated_apim.js
+++ simulated_apim.js
@@ -1,8 +1,13 @@
 'use strict';
 
-const fs   = require("fs");
-const os   = require("os");
+const fs = require("fs");
+const os = require("os");
 const util = require('util');
+const events = require('events');
+const azure  = require('azure');
+
+//var EventHubClient = require('azure-event-hubs').Client;
+const { EventHubClient } = require('@azure/event-hubs');
 
 if (process.argv.length < 5) {
     console.log(process.argv);
@@ -20,6 +25,13 @@ console.log('ms_sleep:  ' + ms_sleep);
 console.log('max_count: ' + max_count);
 console.log('send_ind:  ' + send_ind);
 
+const conn_str = process.env["AZURE_EVENTHUB_CONN_STRING"];
+const hub_name = process.env["AZURE_EVENTHUB_HUBNAME"];
+console.log('conn_str: ' + conn_str);
+console.log('hub_name: ' + hub_name);
+
+const eh_client = EventHubClient.createFromConnectionString(conn_str, hub_name);
+
 var csv_lines = fs.readFileSync('data/pdx-marathon-2017.csv').toString().split("\n");
 console.log('csv count: ' + csv_lines.length);
 
@@ -30,7 +42,12 @@ function randomInt(max) {
     return (Math.floor(Math.random() * Math.floor(max)) + 1);
 }
 
-setInterval(function(){
+async function sendMessage(msg) {
+    const delivery = await eh_client.send(msg);
+    console.log("message sent:\n" + JSON.stringify(msg, null, 2));
+}
+
+setInterval(function() {
     msg_count = msg_count + 1;
     if (msg_count > max_count) {
         console.log('max_count reached; terminating')
@@ -42,24 +59,30 @@ setInterval(function(){
     else {
       csv_index = csv_index + 1;
     }
-    var evt = {};
+    var msg = {};
     var loc = {};
     var date = new Date();
-    evt['pk']  = 'app' + randomInt(10);
-    evt['seq']    = msg_count;
-    evt['date']   = date.toISOString();
-    evt['epoch']  = date.getTime();
-    evt['temperature'] = 20 + (Math.random() * 15);
-    evt['humidity']    = 60 + (Math.random() * 20);
+    msg['pk']  = 'app' + randomInt(10);
+    msg['seq']    = msg_count;
+    msg['date']   = date.toISOString();
+    msg['epoch']  = date.getTime();
+    msg['temperature'] = 20 + (Math.random() * 15);
+    msg['humidity']    = 60 + (Math.random() * 20);
 
     // seq,time,lat,lng,alt_meters,dist_meters,dist_miles
     var csv_fields = csv_lines[csv_index].split('|');
     loc['type'] = 'Point';
     loc['coordinates'] = [Number(csv_fields[3]), Number(csv_fields[2])];
-    evt['location']    = loc;
-    evt['dist_meters'] = csv_fields[5];
-    evt['alt_meters']  = csv_fields[4];
-    
-    console.log("sample message:\n" + JSON.stringify(evt, null, 2));
+    msg['location']    = loc;
+    msg['dist_meters'] = csv_fields[5];
+    msg['alt_meters']  = csv_fields[4];
 
+    if (send_ind === 'send') {
+        sendMessage(msg);
+    }
+    else {
+        console.log("sample message:\n" + JSON.stringify(msg, null, 2));
+    }
 }, ms_sleep);
+
+
